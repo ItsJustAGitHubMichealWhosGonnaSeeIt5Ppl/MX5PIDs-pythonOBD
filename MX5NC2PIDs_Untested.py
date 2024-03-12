a01cf9a81a93900b4e4c80b2142b8b8f6655d64a
BritishCopsAreClowns <finnryanit@gmail.com>
2024-03-12 22:40:34 +0000

    Minor changes

--- MX5NC2PIDs_Untested.py
+++ MX5NC2PIDs_Untested.py
@@ -43,8 +43,8 @@ MX5_BRK_SW = OBDCommand("MX5 Brake Switch",# - Needs testing
 MX5_NEUTRAL_SW = OBDCommand("Park/Neutral Position Switch",# - Needs testing
     "On/Off Park/Neutral detection",  # description
     b"22a211",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
+    5,              
+    raw,            
     ECU.ALL,        
     True,           
     b'7e0')         
@@ -52,8 +52,8 @@ MX5_NEUTRAL_SW = OBDCommand("Park/Neutral Position Switch",# - Needs testing
 MX5_TIRE_TEMP = OBDCommand("Tire Temperture sensor",# - Needs testing
     "Tire temp reading for all 4 wheels",  # description
     b"22c902",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
+    5,              
+    raw,            
     ECU.ALL,        
     True,           
     b'720')         
@@ -61,8 +61,8 @@ MX5_TIRE_TEMP = OBDCommand("Tire Temperture sensor",# - Needs testing
 MX5_CC_V = OBDCommand("Cruise Control Voltage",# - Needs testing
     "Cruise Control voltage reading",  # description
     b"22a216",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
+    5,              
+    raw,            
     ECU.ALL,        
     True,           
     b'7e0')         
@@ -70,8 +70,8 @@ MX5_CC_V = OBDCommand("Cruise Control Voltage",# - Needs testing
 MX5_AC_REFRG_SW = OBDCommand("AC Refrigerant Switch ",# - Needs testing
     "On/Off A/C Refrigerant Pressure Switch",  # description
     b"221104",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
+    5,              
+    raw,            
     ECU.ALL,        
     True,           
     b'7e0')         
@@ -79,8 +79,8 @@ MX5_AC_REFRG_SW = OBDCommand("AC Refrigerant Switch ",# - Needs testing
 MX5_AC_RELAY = OBDCommand("AC Relay",# - Needs testing
     "On/Off A/C Relay",  # description
     b"221101",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
+    5,              
+    raw,            
     ECU.ALL,        
     True,           
     b'7e0')         
@@ -88,8 +88,8 @@ MX5_AC_RELAY = OBDCommand("AC Relay",# - Needs testing
 MX_5_ACCL_PDL = OBDCommand("Full range accelerator pedal",# - Needs testing
     "Accel Pedal Position (full range)",  # description
     b"221340",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
+    5,              
+    raw,            
     ECU.ALL,        
     True,           
     b'7e0')         
@@ -97,8 +97,8 @@ MX_5_ACCL_PDL = OBDCommand("Full range accelerator pedal",# - Needs testing
 MX5_BRKCLTCH_PRES_SW = OBDCommand("Brake Pressure Applied Switch + Clutch position switch",# - Needs testing
     "On/Off Brake Pressure Applied + Clutch Pedal Position Switch. Clutch is Byte 0, Brake is byte 1",  # description
     b"22a211",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
+    5,              
+    raw,            
     ECU.ALL,        
     True,           
     b'7e0') 
@@ -106,17 +106,17 @@ MX5_BRKCLTCH_PRES_SW = OBDCommand("Brake Pressure Applied Switch + Clutch positi
 MX5_INGEAR_SW = OBDCommand("In gear switch",# - Needs testing
     "On/Off In gear switch",  # description
     b"221101",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
-    ECU.ALL,        # (optional) ECU filter
-    True,           # (optional) allow a "01" to be added for speed 
-    b'7e0')         # Header if not 7E0, no 0x
+    5,              
+    raw,            
+    ECU.ALL,     
+    True,         
+    b'7e0')        
 
 MX5_TIRE_RPM = OBDCommand("Tire Revolutions Per Mile(I have no idea)",# - Needs testing
     "Revolutions Per Mile, alleged math ((A*256)+B)",  # description
     b"2216f0",        # Mode+PID, no 0x
-    5,              # number of return bytes to expect and grab
-    raw,            # decoder function
-    ECU.ALL,        # (optional) ECU filter
-    True,           # (optional) allow a "01" to be added for speed 
-    b'7e0')         # Header if not 7E0, no 0x
+    5,              
+    raw,            
+    ECU.ALL,        
+    True,           
+    b'7e0')         
