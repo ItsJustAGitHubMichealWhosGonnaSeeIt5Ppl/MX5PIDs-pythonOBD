da3ae8fb89064e2b625b1ad383fc5f044dbf3668
BritishCopsAreClowns <finnryanit@gmail.com>
2024-03-18 01:25:22 +0000

    Literally one line change

--- MX5NC2Decoders.py
+++ MX5NC2Decoders.py
@@ -7,13 +7,13 @@ from obd.utils import bytes_to_int
 # 7E0 and 7e0 might be different
 
 
-def TPMS(messages):
+def TPMS(messages): # Untested
     """ decoder for TMPS messages """
     fr = messages[0].data # only operate on a single message
     fl = messages[1].data
     rr = messages[2].data
     rl = messages[3].data
-    d = d[2:] # chop off mode and PID bytes
+    d = d[3:] # chop off mode and PID bytes
     v = bytes_to_int(d) / 4.0  # helper function for converting byte arrays to ints
     return v * 0.199136814274 # construct a Pint Quantity
 
